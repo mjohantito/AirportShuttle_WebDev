ddc8aa14271811f4c22ef6e429668819e71fc8ef
Kim Yong-San <mjohantito@student.ciputra.ac.id>
2021-12-06 01:22:09 +0700

    besok lagi gaisss
    okee

--- resources/views/forgotPassword.blade.php
+++ resources/views/forgotPassword.blade.php
@@ -2,46 +2,29 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css" >
+    <title>Forgot Password | Airport Shuttle</title>
+    <link rel="stylesheet" href="css/forgotpassword.css">
     <script src="https://kit.fontawesome.com/af325f8c09.js" crossorigin="anonymous"></script>
-    <title>Forgot Password</title>
 </head>
 <body>
     <div class="back-btn">
         <i class="fas fa-arrow-circle-left"></i>
     </div>
-    <!-- Ganti new password, se bawah2nya  -->
-    <div class="login"> 
-        <div class="login-content">
-            <img src="images/azhuu.png"/>
-            <h2>New Password</h2>
-            <form method="post" id="frmLogin" action="#">
-                <div class="fields">
-                    <h3>New Password</h3>
-                    <div class="field email">
-                        <input required type="password" name="newPass" id="txtNewPass" placeholder="**********" required />
-                    </div>
-                    <h3>Confirm Password</h3>
-                    <div class="field pass">
-                        <input required type="password" name="pass" id="txtConfirmNewPass" placeholder="**********" required/>
-                    </div> 
-                    
-                    <div class="submit">
-                        <input type="submit" value="Reset Password" />
-                    </div>
+    <div class="ConfirmEmail">
+        <form method="post" id="forgotPassword" action="#">
+            <div class ="fields">
+                <img src="images/azhuu.png"/>
+                <h2>Email Confirmation</h2>
+                <h3>Email</h3>
+                <div class="fieldemail">
+                    <input type="email" name="email" placeholder="Example@mail.com" required/>
                 </div>
-            </form>
-            <div class="underline">
-                <div class="u1"></div>
-                <h4>New to AZHUU?</h4>
-                <div class="u2"></div>
+                <div class="continue">
+                    <input type="submit" value="Continue"/>
+                </div>
             </div>
-            <div class="regis">
-                <a href="#">Create your AZHUU account</a>
-            </div>
-        </div>
     </div>
 </body>
 </html>
--- resources/views/newpassword.blade.php
+++ resources/views/newpassword.blade.php
@@ -4,10 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Password</title>
+    <title>Forgot Password | Airport Shuttle</title>
+    <link rel="stylesheet" href="css/forgotpassword.css">
+    <script src="https://kit.fontawesome.com/af325f8c09.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="back-btn"></div>
-    
+    <!-- Ganti jadi new passworrdddddd -->
+    <div class="back-btn">
+        <i class="fas fa-arrow-circle-left"></i>
+    </div>
+    <div class="ConfirmEmail">
+        <form method="post" id="forgotPassword" action="#">
+            <div class ="fields">
+                <img src="images/azhuu.png"/>
+                <h2>Email Confirmation</h2>
+                <h3>Email</h3>
+                <div class="fieldemail">
+                    <input type="email" name="email" placeholder="Example@mail.com" required/>
+                </div>
+                <div class="continue">
+                    <input type="submit" value="Continue"/>
+                </div>
+            </div>
+    </div>
 </body>
 </html>
