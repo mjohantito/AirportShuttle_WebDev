f9482ad81dbfce9d23c2d949704eaffa0b2a01f0
Kim Yong-San <mjohantito@student.ciputra.ac.id>
2021-12-23 22:51:15 +0700

    oioi

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -5,8 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us | Airport Shuttle</title>
+    <link rel="stylesheet" href="css/aboutus.css">
+    <script src="https://kit.fontawesome.com/af325f8c09.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <div class="back-btn">
+        <i class="fas fa-arrow-circle-left"></i>
+    </div>
+    <div class="Cover">
+        <div class="cover">
+            <img src="images/AboutUs.png"/>
+            
+        </div>
+    <div class="AboutUs">
+        <h2>About Azhuu</h2>
+        <div class="img"></div>
+            <img src="images/azhuu.png"/>
+            <h2>Azhuu Inc.</h2>
+        </div>
+    </div>
+        
+        <div class="bedafont">
+            <h3>SINCE 2021</h3>
+            <p>Azhuu is a leading technology company in Indonesia that provides access for people to find and book various transportation services. As a lifestyle super app in Indonesia, Azhuu has a complete product portfolio including airport shuttle transportation service.</p>
+        </div>
+        <div class="ourpartners">
+            <h2>Our Partners</h2>
+            <h3>Favourite Bus Operators</h3>
+            <p>We work with many favourite bus operators accros Indonesia for the best experience we gave to you!</p>
+            <div class="img1">
+                <img src="images/partner1.png"/>
+            </div>
+            <div class="img2">
+                <img src="images/partner2.png"/>
+            </div>
+            <div class="img3">
+                <img src="images/partner3.png"/>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('profile');
+    return view('aboutus');
 });
 
 Route::get('/forgotPassword', function () {
